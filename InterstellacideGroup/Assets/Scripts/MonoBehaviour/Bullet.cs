b6d8feac7077cae4c0438b04d512ab9fe5eac0e3
sal <coolkiddev24@gmail.com>
2020-10-20 16:52:40 -0500

    added bulltets fired when pressing space bar

---
+++ InterstellacideGroup/Assets/Scripts/MonoBehaviour/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 10f;
+    public Rigidbody2D rb;
+    void Start()
+    {
+        //rigid body move right according to speed
+        rb.velocity = transform.right *  speed;
+    }
+
+    void Update()
+    {
+        
+    }
+
+   void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if collision object has  tag "CanBePickedUp", aka coins, hearts, etc...
+        if (collision.gameObject.CompareTag("Enemy"))
+        {   
+            print("HIT ENEMY");
+
+
+        }
+    }
+}
