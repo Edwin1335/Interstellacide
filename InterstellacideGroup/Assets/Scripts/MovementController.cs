92690353616275abee22b286138c3f1cf903f1bc
sal <coolkiddev24@gmail.com>
2020-10-19 23:08:20 -0500

    added temp player sprite, movement, and animations

---
+++ InterstellacideGroup/Assets/Scripts/MovementController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public float movementSpeed = 3.0f;
+    Vector2 movement = new Vector2();
+
+    Animator animator;
+
+    string animationState = "AnimationState";
+    Rigidbody2D rb2D;
+
+
+/* used for animation state triggers */
+    enum CharStates
+    {
+        walkEast = 1,
+        walkSouth = 2,
+        walkWest = 3,
+        walkNorth = 4,
+        idleSouth = 5
+    }
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        UpdateState();
+    }
+
+    void FixedUpdate()
+    {
+        MoveCharacter();
+    }
+
+    private void MoveCharacter()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement.Normalize();
+        rb2D.velocity = movement * movementSpeed;
+    }
+
+/* movements will set animationState to given direction player is walking in, causing transition to animation */
+    private void UpdateState()
+    {
+        if (movement.x > 0)
+        {
+            animator.SetInteger(animationState, (int)CharStates.walkEast);
+        }
+        else if (movement.x < 0)
+        {
+            animator.SetInteger(animationState, (int)CharStates.walkWest);
+        }
+        else if (movement.y > 0)
+        {
+            animator.SetInteger(animationState, (int)CharStates.walkNorth);
+        }
+        else if (movement.y < 0)
+        {
+            animator.SetInteger(animationState, (int)CharStates.walkSouth);
+        }
+        else
+        {
+            animator.SetInteger(animationState, (int)CharStates.idleSouth);
+        }
+    }
+}
