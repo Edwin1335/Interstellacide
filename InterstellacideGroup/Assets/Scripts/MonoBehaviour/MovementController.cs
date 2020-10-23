874b85e0f6064d79b18f316e8d5074980b726080
Edwin Campuzano <campuzano.edwin1995@gmail.com>
2020-10-23 06:48:24 -0500

    Removed Animator from Movement Script

--- InterstellacideGroup/Assets/Scripts/MonoBehaviour/MovementController.cs
+++ InterstellacideGroup/Assets/Scripts/MonoBehaviour/MovementController.cs
@@ -6,32 +6,18 @@ public class MovementController : MonoBehaviour
 {
     public float movementSpeed = 3.0f;
     Vector2 movement = new Vector2();
-    // private bool facingRight;
-    Animator animator;
 
     string animationState = "AnimationState";
     Rigidbody2D rb2D;
 
-
-/* used for animation state triggers */
-    enum CharStates
-    {
-        walkEast = 1,
-        walkSouth = 2,
-        walkWest = 3,
-        walkNorth = 4,
-        idleSouth = 5
-    }
-
     private void Start()
     {
-        animator = GetComponent<Animator>();
         rb2D = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        UpdateState();
+
     }
 
     void FixedUpdate()
@@ -48,32 +34,4 @@ public class MovementController : MonoBehaviour
         rb2D.velocity = movement * movementSpeed;
     }
 
-/* movements will set animationState to given direction player is walking in, causing transition to animation */
-    private void UpdateState()
-    {
-        if (movement.x > 0)
-        {
-            animator.SetInteger(animationState, (int)CharStates.walkEast);
-        }
-        else if (movement.x < 0)
-        {
-            animator.SetInteger(animationState, (int)CharStates.walkWest);
-        }
-        else if (movement.y > 0)
-        {
-            animator.SetInteger(animationState, (int)CharStates.walkNorth);
-        }
-        else if (movement.y < 0)
-        {
-            animator.SetInteger(animationState, (int)CharStates.walkSouth);
-        }
-        else
-        {
-            animator.SetInteger(animationState, (int)CharStates.idleSouth);
-        }
-    }
-
-    // private void Flip(){
-
-    // }
 }
