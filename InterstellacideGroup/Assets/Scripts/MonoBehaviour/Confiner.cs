8e124fb0b82a78bd7c67804261ab5f02b2458e57
Salman Hoosein <salmanhoosein13@gmail.com>
2020-11-14 07:11:51 -0600

    fixed instantiate issue, and added scenes consistency

--- InterstellacideGroup/Assets/Scripts/MonoBehaviour/Confiner.cs
+++ InterstellacideGroup/Assets/Scripts/MonoBehaviour/Confiner.cs
@@ -4,38 +4,39 @@ using UnityEngine;
 
 public class Confiner : Character
 {
-    public HealthBar healthBarPrefab;
-    public StarDustBar starDustBarPrefab;
-    public Player EnesPrefab;
+    // public HealthBar healthBarPrefab;
+    // public StarDustBar starDustBarPrefab;
+    // public Player EnesPrefab;
 
-    Player Enes;
-    HealthBar healthBar;
-    StarDustBar starDustBar;
+    public HealthBar healthBar;
+    public StarDustBar starDustBar;
+    public Player Enes;
 
-    public void Start()
-    {
-        // DontDestroyOnLoad(healthBar);
-        // DontDestroyOnLoad(starDustBar);
-        // DontDestroyOnLoad(Enes);
-        DontDestroyOnLoad(this.gameObject);
-    }
+
+    // public void Start()
+    // {
+    //     hitPoints.value = startingHitPoints;
+    //     startDustPoints.value = startingStarDustPoints;
+
+    //     healthBar.character = this;
+    //     starDustBar.character = this;
+    //     Enes.enes = this;
+    // }
 
     void Awake()
     {
         hitPoints.value = startingHitPoints;
         startDustPoints.value = startingStarDustPoints;
-
-
-        healthBar = Instantiate(healthBarPrefab);
-        starDustBar = Instantiate(starDustBarPrefab);
-        Enes = Instantiate(EnesPrefab);
-        DontDestroyOnLoad(this.gameObject);
-
-
-
+        
         healthBar.character = this;
         starDustBar.character = this;
         Enes.enes = this;
-    
+
+        // healthBar = Instantiate(healthBarPrefab);
+        // starDustBar = Instantiate(starDustBarPrefab);
+        // Enes = Instantiate(EnesPrefab);
+        DontDestroyOnLoad(this.gameObject);
+
+
     }
 }
